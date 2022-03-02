ab1e2d4774cd56e956477c111271358130667bb9
Aurora Misic <misicaurora@gmail.com>
2022-03-02 15:10:59 +0100

    updated score display

--- the-coral-foxes/src/components/Score.js
+++ the-coral-foxes/src/components/Score.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 
 const Score = ({taskScore, totalScore}) => {
-
+    const [ScoreDisplay, setScoreDisplay] = useState(0);
 
     return(
+        
         <div style ={{
             backgroundColor: "black",
             color: "white",
-            width: "10%",
+            width: "20%",
+            fontSize: "50px",
         }}
         >
             Score = {totalScore}
