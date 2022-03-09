055ce8989b7e1d3f2e3fc152a38ec61eb7a501ca
Andreas Winther Moen <andreas@winthermoen.no>
2022-03-09 11:15:17 +0100

    add score to end screen

--- the-coral-foxes/src/components/EndScreen.js
+++ the-coral-foxes/src/components/EndScreen.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Button from "@mui/material/Button";
 
-const EndScreen = ({onEndGame}) => {
+const EndScreen = ({ onEndGame, score }) => {
+  console.log(score);
   return (
     <div
       style={{
@@ -9,12 +10,13 @@ const EndScreen = ({onEndGame}) => {
         marginTop: "200px",
       }}
     >
-      <h2>No more questions</h2>
-      <div>
-                    <Button variant="contained" onClick={onEndGame}>
-                        Front Page
-                    </Button>
-      </div>
+      <h2>Takk for oss!</h2>
+      <h2>Score: {score}</h2>
+      {/* <div>
+        <Button variant="contained" onClick={onEndGame}>
+          Front Page
+        </Button>
+      </div> */}
     </div>
   );
 };
--- the-coral-foxes/src/pages/Gamepage.js
+++ the-coral-foxes/src/pages/Gamepage.js
@@ -7,7 +7,7 @@ import EndScreen from "../components/EndScreen";
 import KahootGameMode from "../components/gameModes/KahootGameMode";
 import PlaceMarkerGameMode from "../components/gameModes/PlaceMarkerGameMode";
 import calculateScore from "../utils/calculateScore";
-import Score from "../components/Score"
+import Score from "../components/Score";
 import SliderKahootGameMode from "../components/gameModes/SliderKahootGameMode";
 
 const Gamepage = ({ onEndGame }) => {
@@ -19,16 +19,15 @@ const Gamepage = ({ onEndGame }) => {
   useEffect(() => setTask(tasks[index]), [index]);
 
   const onSubmitAnswer = async (answer) => {
-    const newScore = Math.round(calculateScore(task, answer))
+    const newScore = Math.round(calculateScore(task, answer));
     setTaskScore(newScore);
     setScore(totalScore + newScore);
     setTimeout(nextTask, 2000);
-
   };
 
   const nextTask = () => setIndex(index + 1);
 
-  if (!task) return <EndScreen />;
+  if (!task) return <EndScreen score={totalScore} />;
 
   let gameMode = <div></div>;
 
@@ -37,31 +36,33 @@ const Gamepage = ({ onEndGame }) => {
       gameMode = <FunFactGameMode funFact={task} onNextTask={nextTask} />;
       break;
     case gameModes.slider:
-      gameMode =  <SliderGameMode task={task} onSubmitAnswer={onSubmitAnswer} />;
+      gameMode = <SliderGameMode task={task} onSubmitAnswer={onSubmitAnswer} />;
       break;
     case gameModes.kahoot:
-      gameMode =  <KahootGameMode task={task} onSubmitAnswer={onSubmitAnswer} />;
+      gameMode = <KahootGameMode task={task} onSubmitAnswer={onSubmitAnswer} />;
       break;
     case gameModes.sliderkahoot:
-      gameMode = <SliderKahootGameMode task={task} onSubmitAnswer={onSubmitAnswer} />;
-    break;
+      gameMode = (
+        <SliderKahootGameMode task={task} onSubmitAnswer={onSubmitAnswer} />
+      );
+      break;
     case gameModes.placeMarker:
       gameMode = (
         <PlaceMarkerGameMode task={task} onSubmitAnswer={onSubmitAnswer} />
-      )
+      );
       break;
     default:
       console.log(`could not find game mode ${task.gameMode}`);
-      gameMode = <EndScreen />;
+      gameMode = <EndScreen score={totalScore} />;
       break;
   }
 
   return (
     <>
-      <Score taskScore = {taskScore} totalScore={totalScore} /> 
+      <Score taskScore={taskScore} totalScore={totalScore} />
       {gameMode};
-    </> 
-  )
+    </>
+  );
 };
 
 export default Gamepage;
